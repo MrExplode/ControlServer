372f12ac26d5de9ba0771c04c09ae4496fc8d02a
MrExplode <pjanos@outlook.hu>
2019-11-08 21:58:20 +0100

    Complete UDP part
    The UDP client/server pair is complete, the code should be reformatted (looks shit)
    Temporary addition of the JoystickTest and JFrameWindow.
    
    Correct pom.xml now

--- src/main/java/me/mrexplode/controlserver/Main.java
+++ src/main/java/me/mrexplode/controlserver/Main.java
@@ -1,11 +1,21 @@
 package me.mrexplode.controlserver;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import me.mrexplode.consolestarter.ConsoleStarter;
+import net.java.games.input.Component;
+import net.java.games.input.Component.Identifier;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
 
 public class Main {
     
@@ -14,42 +24,76 @@ public class Main {
     private byte[] buffer = new byte[64];
     private boolean server = true;
     
+    private String ip = "127.0.0.1";
+    private int port = 42069;
+    private static String controllerName = "Generic   USB  Joystick";
+    
     
     public static void main(String[] args) {
-        //new ConsoleStarter("ControlServer", "args goes here").start();
+        new ConsoleStarter("ControlServer", "server", "127.0.0.1", "42069").start();
         System.out.println("Starting up ControlServer...");
-        float[] floatA = {1.1f, 2.2f};
-        for (float f : floatA) {
-            System.out.print(f + " ");
-        }
-        System.out.println();
-        byte[] byteA = float2byteArray(floatA);
-        for (byte b : byteA) {
-            System.out.print(b + " ");
-        }
-        System.out.println();
-        
-        float[] floatB = byte2floatArray(byteA);
-        for (float f : floatB) {
-            System.out.print(f + " ");
-        }
-        //new Main().run();
+        //loadLibraries("D:\\pjano\\Documents\\Eclipse\\git\\lib", ".dll");
+        new Main(args[0].equals("server"), args[1], Integer.valueOf(args[2])).run();
+    }
+    
+    public Main(boolean server, String ip, int port) {
+        this.server = server;
+        this.ip = ip;
+        this.port = port;
     }
 
     private void run() {
         try {
-            DatagramSocket socket = new DatagramSocket(69420);
+            ArrayList<Controller> gameControllers = (ArrayList<Controller>) Arrays.asList(ControllerEnvironment.getDefaultEnvironment().getControllers())
+                                                    .stream().filter(x -> (x.getType() == Controller.Type.GAMEPAD) || x.getType() == Controller.Type.WHEEL || x.getType() == Controller.Type.STICK).collect(Collectors.toList());
             
-            while (true) {
-                if (server) {
-                    //send data
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                } else {
-                    //recieve data
-                }
+            DatagramSocket socket = new DatagramSocket((server ? port -1 : port));
+            long timer = 0;
+            long statTimer = 0;
+            if (server) {
+                Controller controller = gameControllers.get(0);
+                Component comp1 = controller.getComponent(Identifier.Axis.X);
+                Component comp2 = controller.getComponent(Identifier.Axis.RZ);
                 
+                while (true) {
+                  //25ms sending time
+                    if (System.currentTimeMillis() - timer > 50) {
+                        timer = System.currentTimeMillis();
+                        
+                        //poll controller data
+                        if (!controller.poll()) {
+                            System.out.println("Controller disconnected!\nStopping...");
+                            break;
+                        }
+                        
+                        this.turn = comp1.getPollData();
+                        this.throttle = comp2.getPollData();
+                        
+                        //send data
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(ip), port);
+                        packet.setData(float2byteArray(turn, throttle));
+                        socket.send(packet);
+                    }
+                    
+                    //stat timing, 1 sec
+                    if (System.currentTimeMillis() - statTimer > 1000) {
+                        statTimer = System.currentTimeMillis();
+                        System.out.println("Sent data to [" + ip + ":" + port + "]: turn=" + turn + " throttle=" + throttle);
+                    }
+                }
+            } else {
+                //recieve data
+                while (true) {
+                    timer = System.currentTimeMillis();
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                    socket.receive(packet);
+                    long recInterval = System.currentTimeMillis() - timer;
+                    float[] data = byte2floatArray(packet.getData());
+                    System.out.println("Recieved data from [" + packet.getAddress().getHostAddress() + ":" + packet.getPort() + "] turn=" + data[0] + " throttle=" + data[1] + " packet interval=" + recInterval + "ms");
+                }
             }
-        } catch (SocketException e) {
+            socket.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -70,6 +114,17 @@ public class Main {
         float[] fa = new float[fb.limit()];
         fb.get(fa);
         return fa;
+    }
+    
+    private static void loadLibraries(String folder, String type) {
+        String[] files = new File(folder).list();
+        for (String file : files) {
+            if (file.endsWith(type)) {
+                System.out.println("Loading " + file);
+                System.load(folder + "\\" + file);
+            }
+        }
+        System.out.println("Successfully loaded libraries!");
     }
 
 }
